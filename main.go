936173058cd77025b1a18552fe02bb81dee6361a
Jacky Tang <jackytck@gmail.com>
2017-10-21 00:02:30 +0800

    :construction: embedded struct + pointer receiver

--- main.go
+++ main.go
@@ -2,23 +2,52 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+// embeded struct
 type person struct {
 	firstName string
 	lastName  string
+	// contact   contactInfo
+	contactInfo
+}
+
+// p is pass by value
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
 }
 
 func main() {
-	// 1. implicit order
-	alex := person{"Alex", "Anderson"}
-	fmt.Printf("%+v\n", alex)
+	// // 1. implicit order
+	// alex := person{"Alex", "Anderson"}
+	// fmt.Printf("%+v\n", alex)
 
-	// 2. field names
-	alex2 := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Printf("%+v\n", alex2)
+	// // 2. field names
+	// alex2 := person{firstName: "Alex", lastName: "Anderson"}
+	// fmt.Printf("%+v\n", alex2)
 
-	// 3. zero value
-	var alex3 person
-	alex3.firstName = "Alex"
-	alex3.lastName = "Anderson"
-	fmt.Printf("%+v\n", alex3)
+	// // 3. zero value
+	// var alex3 person
+	// alex3.firstName = "Alex"
+	// alex3.lastName = "Anderson"
+	// fmt.Printf("%+v\n", alex3)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	jim.updateName("jimmy")
+	jim.print()
 }
